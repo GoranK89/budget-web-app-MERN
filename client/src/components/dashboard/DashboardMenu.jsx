71069036539d82e53e15ffca34e088bb3903d7aa
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-04-18 22:10:48 +0200

    refactored dashboard menu for clarity

--- client/src/components/dashboard/DashboardMenu.jsx
+++ client/src/components/dashboard/DashboardMenu.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 import IconDashboard from "/icons/dashboard.png";
 import IconPiggyBank from "/icons/piggy-bank.png";
 import IconBrokenPiggyBank from "/icons/broke-piggy-bank.png";
 import IconChart from "/icons/combo-chart.png";
 import IconInvestment from "/icons/investment.png";
 
+const MENU_ITEMS = [
+  { id: "dashboard", label: "Dashboard", icon: IconDashboard },
+  { id: "income", label: "Income", icon: IconPiggyBank },
+  { id: "expenses", label: "Expenses", icon: IconBrokenPiggyBank },
+  { id: "charts", label: "Charts", icon: IconChart },
+  { id: "investments", label: "Investments", icon: IconInvestment },
+];
+
 const DashboardMenu = () => {
+  const [active, setActive] = useState("dashboard");
+
+  const handleActive = (menuItem) => {
+    setActive(menuItem.target.innerText.toLowerCase());
+  };
+
   return (
     <section className="section-menu">
       <div className="balance-box">
@@ -12,26 +28,15 @@ const DashboardMenu = () => {
         <p>Current balance</p>
       </div>
       <div className="menu-box">
-        <div className="menu-box__item">
-          <img src={IconDashboard} />
-          <a href="#">Dashboard</a>
-        </div>
-        <div className="menu-box__item">
-          <img src={IconPiggyBank} />
-          <a href="#">Income</a>
-        </div>
-        <div className="menu-box__item">
-          <img src={IconBrokenPiggyBank} />
-          <a href="#">Expenses</a>
-        </div>
-        <div className="menu-box__item">
-          <img src={IconChart} />
-          <a href="#">Charts</a>
-        </div>
-        <div className="menu-box__item">
-          <img src={IconInvestment} />
-          <a href="#">Investments</a>
-        </div>
+        {MENU_ITEMS.map((item) => (
+          <div
+            className={`menu-box__item ${active === item.id ? "active" : ""}`}
+            onClick={handleActive}
+          >
+            <img src={item.icon} />
+            <a href="#">{item.label}</a>
+          </div>
+        ))}
       </div>
     </section>
   );
